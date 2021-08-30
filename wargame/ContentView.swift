da26d2c0d3ef3d325766de08c3ed7aacc63bcbd9
Andy <and.hammond12@gmail.com>
2021-08-30 10:20:30 -0400

    Initial Commit

---
+++ wargame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wargame
+//
+//  Created by Andy Hammond on 2021-08-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wargame/wargameApp.swift
@@ -0,0 +1,17 @@
+//
+//  wargameApp.swift
+//  wargame
+//
+//  Created by Andy Hammond on 2021-08-30.
+//
+
+import SwiftUI
+
+@main
+struct wargameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
