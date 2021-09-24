e1505c16e4d6123427ae14a635ec84d5b9de4f21
Andy <and.hammond12@gmail.com>
2021-09-24 13:59:08 -0400

    update content view

--- wargame/ContentView.swift
+++ wargame/ContentView.swift
@@ -8,9 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var playerCard = "card6"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack{
+            Image("background")
+            
+           
+            VStack{
+                Spacer()
+                Image("logo")
+                
+                Spacer()
+                HStack(spacing: 12){
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+            }
+                .padding()
+                Spacer()
+                //deal button
+                Button(action: {
+                    
+                   // Generate a random number between 2 and 13
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else {
+                        cpuScore += 1
+                    }
+                    
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    // update player score
+                    
+                    
+                    
+                   
+                }, label: {
+                    Image("dealbutton")
+                })
+                
+                
+                Spacer()
+                //player scores
+                
+                HStack {
+                   Spacer()
+                    VStack(spacing: 12){
+                        Text("Player 1")
+                            .font(.title)
+                            .foregroundColor(.white)
+                        Text(String(playerScore))
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    VStack(spacing: 12){
+                        Text("CPU")
+                            .font(.title)
+                            .foregroundColor(.white)
+                        Text(String(cpuScore))
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }
+                    Spacer()
+                    
+                }
+                Spacer()
+            }
+           
+        }
+       
+        
     }
 }
 
